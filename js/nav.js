843e0f855ead158c0acff17624d979fc40582ce1
Guiem Portàtil <guillembagurmoll@gmail.com>
2024-10-17 17:18:43 +0200

    soem changes

---
+++ js/nav.js
@@ -0,0 +1,7 @@
+const expandNavBtn = document.getElementById("expand-nav-btn");
+const navLinks = document.getElementById("nav-links");
+
+expandNavBtn.addEventListener("click", function() {
+    navLinks.classList.toggle("nav__links--hidden");
+    console.log(navLinks.classList);
+})
