8b4aee765290abe3e58cb13e92f2f47a3aac27f9
Guiem Portàtil <guillembagurmoll@gmail.com>
2024-11-15 16:53:02 +0100

    normalize folder structure, add required changes to index file

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/views/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "darkblue": "#0A0F14",
+        "brown": "#1E1E1E",
+        "gray": "#464646",
+        "white": "#FFE6D7",
+        "blue": "#192832",
+        "orange": "#BE6900",
+      },
+
+      fontFamily: {
+        "serif": ["'Libre Baskerville'", "'Times New Roman'", "serif"],
+      }
+    },
+  },
+  plugins: [],
+}
+
